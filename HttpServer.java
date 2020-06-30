86908627c5ec7a9c37fd2de84adedb2186cf1cd2
pmasharov <2386444@gmail.com>
2020-06-30 17:47:20 +0300

    added simple form inputs

--- HttpServer.java
+++ HttpServer.java
@@ -11,28 +11,30 @@ public class HttpServer {
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(8080)) {
       System.out.println("Server started!");
-   
-      while(true) {
+
+      while (true) {
         Socket socket = serverSocket.accept();
         System.out.println("Client connected!");
         try (
-          BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-          PrintWriter output = new PrintWriter(socket.getOutputStream())
-          ) {
-            while(!input.ready());
+            BufferedReader input = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            PrintWriter output = new PrintWriter(socket.getOutputStream())) {
+          while (!input.ready())
+            ;
 
-            System.out.println();
-            while(input.ready()) {
-              System.out.println(input.readLine());
-            }
+          System.out.println();
+          while (input.ready()) {
+            System.out.println(input.readLine());
+          }
 
-            output.println("HTTP/1.1 200 OK");
-            output.println("Content-Type: text/html; charset=utf-8");
-            output.println();
-            output.println("<p>Hi to All!</p>");
-            output.flush();
+          output.println("HTTP/1.1 200 OK");
+          output.println("Content-Type: text/html; charset=utf-8");
+          output.println();
+          output.println(
+              "<h1>Дратути</h1><h3>Хренак-хренак и готово</h3><input type='text' name='name' /><br><button name='button'>Жмак нах</button>");
+          output.flush();
 
-            System.out.println("Client disconnected!");
+          System.out.println("Client disconnected!");
         }
       }
     } catch (IOException exception) {
